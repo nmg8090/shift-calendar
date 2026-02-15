548b071641646c66ab4237d7f8cd250772c54040
nmg8090 <nmg8090@gmail.com>
2026-02-16 08:25:34 +0900

    Major upgrade: glassmorphism UI, notes, PWA, holidays, week view, theme toggle, animations

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'shift-cal-v2';
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
